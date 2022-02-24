ac470133948639ca6f7ad43b279d729d107be11c
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-24 10:20:27 -0800

    adding Program file

---
+++ watch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace StopWatch
+{
+    public class watch
+    {
+        public static void Watch()
+
+        {
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+            stopwatch.Start();
+
+            for (int i=0; i<=1000; i++)
+
+            {
+                Thread.Sleep(10);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine("Time Elapsed" + stopwatch.Elapsed);
+
+
+        }
+    }
+}
