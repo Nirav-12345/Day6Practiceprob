68defd31ad2164fac8f3a764ef2731e09a0ce4f0
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-24 10:12:33 -0800

    adding Program file

---
+++ FEboSer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FebonaciSeries
+{
+    public class FEboSer
+    {
+        public static void FSeri()
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (i=2; i<=number; i++)
+            {
+                n3 = n1 + n2;
+                n1 = n2;  n2 = n3;
+
+                Console.WriteLine(n3);
+
+            }
+
+        }
+    }
+}
