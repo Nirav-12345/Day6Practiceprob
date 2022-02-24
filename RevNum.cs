daf8530a861c43cef9dbb8561d145decc8bde86a
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-24 10:17:45 -0800

    adding Program file

---
+++ RevNum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAnumber
+{
+    public class RevNum
+    {
+
+
+        public static void RNum()
+
+        {
+            int n, reverse = 0, rem;
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while(n!=0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+
+                n /= 10;
+
+                Console.WriteLine(reverse);
+            }
+        }
+    }
+}
