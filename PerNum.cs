6f1cc5f375e586727d9763a4e2cfd659d25597dc
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-24 10:11:18 -0800

    adding program file

---
+++ PerNum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectNumber
+{
+    public class PerNum
+    {
+
+        public static void PerfeN()
+        {
+            int i, n, sum;
+
+            int mn, mx;
+            mn = Convert.ToInt32(Console.ReadLine());
+            mx = Convert.ToInt32(Console.ReadLine());
+
+            for (n = mn; n <= mx; n++)
+            {
+
+                i = 1;
+                sum = 0;
+
+                while (i < n)
+                {
+                    if (n % i == 0)
+                    
+                        sum = sum + i;
+
+                        i++;
+                    
+                }
+
+                if(sum==n)
+                
+                    Console.WriteLine(n);
+                
+            }
+        }
+    }
+}
